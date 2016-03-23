5d6a5c7c6e8876d8c3ae0ac96846e6ea6aaca9a6
wgsjack199213 <wgsjack199213@yeah.net>
2016-03-23 15:43:44 +0800

    csrf, check wechat token, get url paras

--- collect_news/settings.py
+++ collect_news/settings.py
@@ -43,7 +43,7 @@ MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -81,6 +81,7 @@ DATABASES = {
     }
 }
 
+TIME_ZONE = 'Asia/Shanghai'
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
--- collector/views.py
+++ collector/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
+import hashlib
 
 # try logging
 import logging
@@ -9,4 +11,26 @@ logger = logging.getLogger(__name__)
 
 def index(request):
     logger.info(str(request))
+
     return HttpResponse("Hello, world. Zootopia is so great!")
+
+
+@csrf_exempt
+def receive_link(request):
+    logger.info(str(request))
+    paras = request.GET
+
+    # Decide if the request is from Weixin
+    nonce = paras['nonce']
+    timestamp = paras['timestamp']
+    signature = paras['signature']
+    token = 'wangguosai'
+    parameters = sorted([nonce, timestamp, token])
+    sha1 = hashlib.sha1(''.join(parameters)).hexdigest()
+    if not sha1 == signature:
+        return HttpResponse("Goodbye!")
+    
+    logger.info('Welcome here, Wechat!')
+    
+
+    return HttpResponse("Hello!")
